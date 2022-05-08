5ed73b99064d470fd925ca493768ca5d5228f325
waleedo0o7 <waleedhd1@gmail.com>
2022-05-08 20:13:15 +0200

    fix datepicker and add css file again

--- header.php
+++ header.php
@@ -34,7 +34,7 @@
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
 
     <!-- jquery UI  -->
-    <!-- <link href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" rel="stylesheet" type="text/css" /> -->
+    <link href="css/jquery-ui.css" rel="stylesheet" type="text/css" />
 
     <!-- Custom Style -->
     <link rel="stylesheet" type="text/css" href="css/style.css">
